38376fc9731caf842af59417675b5ba4baf56a62
astralium5 <vuttikorn5256@gmail.com>
2026-05-16 11:10:23 +0700

    Add line trim + detection etc

--- mod/orca.py
+++ mod/orca.py
@@ -7,6 +7,15 @@ import subprocess, re
 cfg_gameid = config.get("launch.place-id")
 cfg_gamedomain = config.get("launch.goto-domain")
 
+cfg_identifier = config.get("detection.rbx-identifier")
+cfg_sliceindex = config.get("detection.slice-index")
+
+cfg_start_identifier = config.get("detection.start-processing-call")
+
+cfg_ignorevalues = config.get("detection.ignore-values")
+
+print(cfg_ignorevalues)
+
 process = subprocess.Popen(
     # the backslash is extremely important (actually)
     ["flatpak", "run", "org.vinegarhq.Sober", f"roblox://placeId={cfg_gameid}\\&launchData={cfg_gamedomain}"],
@@ -16,8 +25,26 @@ process = subprocess.Popen(
     bufsize=1
     )
 
+started_state = False
+
 for line in process.stdout:
     line = line.strip()
 
-    print(line)
+    if cfg_identifier in line:
+        # We got an output line!
+        trimmed_line = line[cfg_sliceindex::]
+
+        if not started_state:
+            if cfg_start_identifier in trimmed_line:
+                # We've started processing the logs
+                started_state = True
+            else:
+                continue
+
+        if any(value in trimmed_line for value in cfg_ignorevalues):
+            # String contains a substring in ignore list
+            continue
+
+        print(trimmed_line)
+
     pass
