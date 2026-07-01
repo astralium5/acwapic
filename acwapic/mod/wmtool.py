a5690a2f1db225a945522a672fbd1f07d1502b18
astralium5 <vuttikorn5256@gmail.com>
2026-07-01 18:44:35 +0700

    (try to) get started on adding windows (THE OPERATING SYSTEM) support

--- acwapic/mod/wmtool.py
+++ acwapic/mod/wmtool.py
@@ -1,41 +1,71 @@
 # Do window manager stuff
-from ewmh import EWMH
 import os, platform
 
-ewmh = EWMH()
+window_manager = None
+os_flavor = platform.system()
+
+match os_flavor:
+    case "Windows":
+        import pywinctl
+        window_manager = pywinctl
+    case "Linux":
+        from ewmh import EWMH
+        window_manager = EWMH()
 
 # Check the current OS.
 def checkos():
-    return platform.system()
+    return os_flavor
 
 # Check the window manager currently running.
 def checkwm():
-    session_type = os.environ.get('XDG_SESSION_TYPE', '').lower()
-    wayland_display = os.environ.get('WAYLAND_DISPLAY')
-    
-    if session_type == 'wayland' or wayland_display:
-        return 2
-    elif session_type == 'x11' or os.environ.get('DISPLAY'):
-        return 1
-    else:
-        return 0
+    match os_flavor:
+        case "Windows":
+            return 1 # NO I AM NOT DEALING WITH THIS STUPID CRAP
+        case "Linux":
+            session_type = os.environ.get('XDG_SESSION_TYPE', '').lower()
+            wayland_display = os.environ.get('WAYLAND_DISPLAY')
+            
+            if session_type == 'wayland' or wayland_display:
+                return 2
+            elif session_type == 'x11' or os.environ.get('DISPLAY'):
+                return 1
+            else:
+                return 0
 
 # Yield until a window of a certain name appears.
 def await_window(name: str) -> int:
-    while True:
-        active_win = ewmh.getActiveWindow()
-        if active_win == None:
-            continue
-        active_name = ewmh.getWmName(active_win)
+    match os_flavor:
+        case "Windows":
+            while True:
+                # In windows the window that appears may not always be in focus immediately
+                windows = window_manager.getWindowsWithTitle(name)
+                if windows:
+                    window = windows[0]
+                    window.activate()
+                    return window
+        case "Linux":
+            while True:
+                active_win = window_manager.getActiveWindow()
+                if active_win == None:
+                    continue
+                active_name = window_manager.getWmName(active_win)
 
-        if active_name == name:
-            return active_win
-    pass
+                if active_name == name:
+                    return active_win
 
 # Setup a window.
-def setup_window(id: int, px: int, py: int, sx: int, sy: int):
-    ewmh.setWmState(id, 0, '_NET_WM_STATE_MAXIMIZED_HORZ')
-    ewmh.setWmState(id, 0, '_NET_WM_STATE_MAXIMIZED_VERT')
-    ewmh.setWmState(id, 0, '_NET_WM_STATE_FULLSCREEN')
-    ewmh.setMoveResizeWindow(id, 0, px, py, sx, sy)
-    ewmh.display.flush()
+def setup_window(window, px: int, py: int, sx: int, sy: int):
+    match os_flavor:
+        case "Windows":
+            # window is a.. window
+            if window.isMaximized:
+                window.restore()
+            window.moveTo(px, py)
+            window.resizeTo(sx, sy)
+        case "Linux":
+            # window is an integer
+            window_manager.setWmState(window, 0, '_NET_WM_STATE_MAXIMIZED_HORZ')
+            window_manager.setWmState(window, 0, '_NET_WM_STATE_MAXIMIZED_VERT')
+            window_manager.setWmState(window, 0, '_NET_WM_STATE_FULLSCREEN')
+            window_manager.setMoveResizeWindow(window, 0, px, py, sx, sy)
+            window_manager.display.flush()
